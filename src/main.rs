9b52b65273ff1b0740c0a9fec9efea50b486c984
Yolkkaii <drifter_2010@outlook.com>
2025-07-09 13:50:11 +0700

    Rust Array, Tuple & Slice

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let arr = [5, 10, 15, 20, 25];
+    let slice = &arr[1..4];
+    let tuple = ("Rust", 2024, true);
+
+    for x in slice {
+        println!("{}", x);
+    };
+
+    println!("{} {} {}", tuple.0, tuple.1, tuple.2);
 }
