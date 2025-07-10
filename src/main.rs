361974f90700d14caf62ced38751f4058206efef
Yolkkaii <drifter_2010@outlook.com>
2025-07-10 22:14:59 +0800

    Rust Edit

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,21 @@
 fn main() {
-    // let arr = [5, 10, 15, 20, 25];
-    // let slice = &arr[1..4];
-    // let tuple = ("Rust", 2024, true);
+    let arr = [5, 10, 15, 20, 25];
+    let slice = &arr[1..4];
+    let tuple = ("Rust", 2024, true);
 
-    // for x in slice {
-    //     println!("{}", x);
-    // };
+    for x in slice {
+        println!("{}", x);
+    };
 
-    // println!("{} {} {}", tuple.0, tuple.1, tuple.2);
+    println!("{} {} {}", tuple.0, tuple.1, tuple.2);
 
-    // let mut _count = 1;
-    // let mut _sum = 0;
+    let mut _count = 1;
+    let mut _sum = 0;
 
-    // while _count <= 10 {
-    //     _sum += _count;
-    //     _count += 1;
-    // }
+    while _count <= 10 {
+        _sum += _count;
+        _count += 1;
+    }
 
-    // println!("{}", _sum)
-
-    
+    println!("{}", _sum)
 }
