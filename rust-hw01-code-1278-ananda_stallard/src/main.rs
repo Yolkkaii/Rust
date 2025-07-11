03629dbf94bdf007da5a74404bfd324decf8e6e9
Yolkkaii <drifter_2010@outlook.com>
2025-07-11 22:04:38 +0800

    Temperature checker: Fix!

--- rust-hw01-code-1278-ananda_stallard/src/main.rs
+++ rust-hw01-code-1278-ananda_stallard/src/main.rs
@@ -23,9 +23,25 @@ fn main() {
             }
         }
 
-        let temp: i32 = input.trim().parse().expect("Input not an integer");
+        //check if the user manually stopped the program
+        let trimmed_input = input.trim();
+
+        if trimmed_input.eq_ignore_ascii_case("stop") {
+            println!("Program manually stopped.");
+            break;
+        }
+
+        //check if invalid input
+        let temp: i32 = match trimmed_input.parse() {
+            Ok(num) => {num},
+            Err(_) => {
+                println!("Invalid input. Please enter a number or type 'Stop' to exit.");
+                continue;
+            }
+        };
 
         check_temp(temp, &mut heater, &mut air_con);
+
         println!("Temperature will be measured again in 5 minutes");
         thread::sleep(Duration::from_secs(5 * 60));
     }
@@ -41,23 +57,23 @@ fn check_temp(value: i32, heat: &mut bool, air_c: &mut bool) {
     //! is like a NOT statement
 
     if value < 18 {
-        if !*heat {
-            *heat = true;
-            println!("Turned on heater!");
-        }
         if *air_c {
             *air_c = false;
             println!("Turned off air conditioner!");
         }
+        if !*heat {
+            *heat = true;
+            println!("Turned on heater!");
+        }
 
     } else if value > 24 {
-        if !*air_c {
-            *air_c = true;
-            println!("Turned on air conditioner!");
-        }
         if *heat {
             *heat = false;
             println!("Turned off heater!");
+        }
+        if !*air_c {
+            *air_c = true;
+            println!("Turned on air conditioner!");
         }
 
     } else {
